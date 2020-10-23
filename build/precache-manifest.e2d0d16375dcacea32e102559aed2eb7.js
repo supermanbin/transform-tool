319268e5d274566c92dcb1580997b6b58f34d904
supermanbin <fengbinsuperman@gmail.com>
2020-10-23 13:46:55 +0800

    build

---
+++ build/precache-manifest.e2d0d16375dcacea32e102559aed2eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4337665e3af0f0774f6ec57b76d0188d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c91011cb800f80a2c40",
+    "url": "/static/css/main.32475eb3.chunk.css"
+  },
+  {
+    "revision": "2157566409f51dede215",
+    "url": "/static/js/2.1621a67c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.1621a67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c91011cb800f80a2c40",
+    "url": "/static/js/main.ffb1d4ad.chunk.js"
+  },
+  {
+    "revision": "c812d07e2e2100dba90f",
+    "url": "/static/js/runtime-main.bce92d24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
